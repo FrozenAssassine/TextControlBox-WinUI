65166144b32711932e9d470f4a5e84c4f763e6b6
Julius Kirsch <Julius@skirsch.de>
2025-11-23 17:17:25 +0100

    fix: asm language regex fixes and color changes

--- TextControlBox/Languages/Languages.cs
+++ TextControlBox/Languages/Languages.cs
@@ -313,14 +313,14 @@
             this.Description = "Syntax highlighting for the original x86 ISA assembly";
             this.Highlights = new SyntaxHighlights[]
             {
-                new SyntaxHighlights("^((?i)rep|^(?i)repe|^(?i)repne|^(?i)repnz|^(?i)repz)($|\\s)", "#e6005c", "#e6005c", true), // prefixes
-                new SyntaxHighlights("((?i)aaa|(?i)aad|(?i)aam|(?i)aas|(?i)adc|(?i)add|(?i)and|(?i)call|(?i)cbw|(?i)clc|(?i)cld|(?i)cli|(?i)cmc|(?i)cmp|(?i)cmpsb|(?i)cmpsw|(?i)cwd|(?i)daa|(?i)das|(?i)dec|(?i)div|(?i)esc|(?i)hlt|(?i)idiv|(?i)imul|(?i)in|(?i)inc|(?i)int|(?i)into|(?i)iret|(?i)ja|(?i)jae|(?i)jb|(?i)jbe|(?i)jc|(?i)je|(?i)jg|(?i)jge|(?i)jl|(?i)jle|(?i)jna|(?i)jnae|(?i)jnb|(?i)jnbe|(?i)jnc|(?i)jne|(?i)jng|(?i)jnge|(?i)jnl|(?i)jnle|(?i)jno|(?i)jnp|(?i)jns|(?i)jnz|(?i)jo|(?i)jp|(?i)jpe|(?i)jpo|(?i)js|(?i)jz|(?i)jcxz|(?i)jmp|(?i)lahf|(?i)lds|(?i)lea|(?i)les|(?i)lock|(?i)lodsb|(?i)lodsw|(?i)loop|(?i)mov|(?i)movsb|(?i)movsw|(?i)mul|(?i)neg|(?i)nop|(?i)not|(?i)or|(?i)out|(?i)pop|(?i)popf|(?i)push|(?i)pushf|(?i)rcl|(?i)rcr|(?i)ret|(?i)retn|(?i)retf|(?i)rol|(?i)ror|(?i)sahf|(?i)sal|(?i)sar|(?i)sbb|(?i)scasb|(?i)scasw|(?i)shl|(?i)shr|(?i)stc|(?i)std|(?i)sti|(?i)stosb|(?i)stosw|(?i)sub|(?i)test|(?i)wait|(?i)xchg|(?i)xlat|(?i)xor)($|\\s)", "#8400ff", "#8400ff", true), // instructions
-                new SyntaxHighlights("(^|\\s|\\[|\\]|\\*|-|\\+)((?i)rax|(?i)eax|(?i)ax|(?i)ah|(?i)al|(?i)rbx|(?i)ebx|(?i)bx|(?i)bh|(?i)bl|(?i)rcx|(?i)ecx|(?i)cx|(?i)ch|(?i)cl|(?i)rdx|(?i)edx|(?i)dx|(?i)dh|(?i)dl|(?i)rdi|(?i)edi|(?i)di|(?i)dil|(?i)rsi|(?i)esi|(?i)si|(?i)sil|(?i)cs|(?i)ds|(?i)ss|(?i)es|(?i)fs|(?i)ds|(?i)rbp|(?i)ebp|(?i)bp|(?i)bpl|(?i)rip|(?i)eip|(?i)ip|(?i)rsp|(?i)esp|(?i)sp|(?i)spl)", "#c4aa00", "#c4aa00"), // registers
-                new SyntaxHighlights("0b|0[xX][0-9a-fA-F]+|[0-9]+|0[bB][0-1]+", "#74bd00", "#74bd00"), // values
-                new SyntaxHighlights("^\\s*(?i)int", "#0067c2", "#0067c2", true), // int
-                new SyntaxHighlights("(\\[|\\]|\\*|\\+|-])", "#0099ff", "#0099ff"), // brackets
-                new SyntaxHighlights("^\\s*[aA-zZ]+:", "#00ab5b", "#00ab5b", italic: true), // labels
-                new SyntaxHighlights("\\s*;(.*)", "#04d400", "#04d400") // comments
+                new SyntaxHighlights("^((?i)rep|^(?i)repe|^(?i)repne|^(?i)repnz|^(?i)repz)($|\\s)", "#5e00c7", "#983cff", true), // prefixes
+                new SyntaxHighlights(@"(?i)\b(aaa|aad|aam|aas|adc|add|and|call|cbw|clc|cld|cli|cmc|cmp|cmpsb|cmpsw|cwd|daa|das|dec|div|esc|hlt|idiv|imul|in|inc|int|into|iret|ja|jae|jb|jbe|jc|je|jg|jge|jl|jle|jna|jnae|jnb|jnbe|jnc|jne|jng|jnge|jnl|jnle|jno|jnp|jns|jnz|jo|jp|jpe|jpo|js|jz|jcxz|jmp|lahf|lds|lea|les|lock|lodsb|lodsw|loop|mov|movsb|movsw|mul|neg|nop|not|or|out|pop|popf|push|pushf|rcl|rcr|ret|retn|retf|rol|ror|sahf|sal|sar|sbb|scasb|scasw|shl|shr|stc|std|sti|stosb|stosw|sub|test|wait|xchg|xlat|xor)($|\s)", "#8400ff", "#8400ff", true), // instructions
+                new SyntaxHighlights(@"(?i)(?<=^|\\s|\\[|\\]|\\*|\\+|-)(rax|eax|ax|ah|al|rbx|ebx|bx|bh|bl|rcx|ecx|cx|ch|cl|rdx|edx|dx|dh|dl|rdi|edi|di|dil|rsi|esi|si|sil|cs|ds|ss|es|fs|rbp|ebp|bp|bpl|rip|eip|ip|rsp|esp|sp|spl)\b", "#c4aa00", "#c4aa00"), // registers
+                new SyntaxHighlights("0b|0[xX][0-9a-fA-F]+|[0-9]+|0[bB][0-1]+", "#558900", "#74bd00"), // values
+                new SyntaxHighlights("^\\s*(?i)int", "#005eb0", "#1994ff", true), // int
+                new SyntaxHighlights("(\\[|\\]|\\*|\\+|-])", "#0075c3", "#0099ff"), // brackets
+                new SyntaxHighlights("^\\s*[aA-zZ]+:", "#007a41", "#00ab5b", italic: true), // labels
+                new SyntaxHighlights("\\s*;(.*)", "#028100", "#04d400") // comments
             };
         }
     }
