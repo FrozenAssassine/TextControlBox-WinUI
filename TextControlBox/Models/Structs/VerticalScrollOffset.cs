0b70ee236c3d5f783c379b1b394f8903b8456a86
Julius Kirsch <Julius@skirsch.de>
2026-01-03 11:43:45 +0100

    ref: simplified verticalScrollOffset struct a bit

--- TextControlBox/Models/Structs/VerticalScrollOffset.cs
+++ TextControlBox/Models/Structs/VerticalScrollOffset.cs
@@ -1,151 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.Foundation;
+﻿namespace TextControlBoxNS.Models.Structs;
 
-namespace TextControlBoxNS.Models.Structs
+/// <summary>
+/// Describes the offset between content and vertical scroll area borders. 
+/// Two Double values describe the Top and Bottom offsets, respectively.
+/// </summary>
+public struct VerticalScrollOffset
 {
     /// <summary>
-    /// Describes the offset between content and vertical scroll area borders. 
-    /// Two Double values describe the Top and Bottom offsets, respectively.
+    /// The top offset of content
     /// </summary>
-    public struct VerticalScrollOffset
+    public double Top { get; set; }
+
+    /// <summary>
+    /// The bottom offset of content
+    /// </summary>
+
+    public double Bottom { get; set; }
+
+    /// <summary>
+    /// Creates new VerticalScrollOffset object
+    /// </summary>
+    /// <param name="uniformLength">The top and bottom content offset</param>
+    public VerticalScrollOffset(double uniformLength)
     {
-        private double _Top;
+        Top = (Bottom = uniformLength);
+    }
 
-        private double _Bottom;
-
-        /// <summary>
-        /// The top offset of content
-        /// </summary>
-        public double Top
-        {
-            get
-            {
-                return _Top;
-            }
-            set
-            {
-                _Top = value;
-            }
-        }
-
-        /// <summary>
-        /// The bottom offset of content
-        /// </summary>
-
-        public double Bottom
-        {
-            get
-            {
-                return _Bottom;
-            }
-            set
-            {
-                _Bottom = value;
-            }
-        }
-
-        /// <summary>
-        /// Creates new VerticalScrollOffset object
-        /// </summary>
-        /// <param name="uniformLength">The top and bottom content offset</param>
-        public VerticalScrollOffset(double uniformLength)
-        {
-            _Top = (_Bottom = uniformLength);
-        }
-
-        /// <summary>
-        /// Creates new VerticalScrollOffset object
-        /// </summary>
-        /// <param name="top">The top offset of content</param>
-        /// <param name="bottom">The bottom offset of content</param>
-        public VerticalScrollOffset(double top, double bottom)
-        {
-            _Top = top;
-            _Bottom = bottom;
-        }
+    /// <summary>
+    /// Creates new VerticalScrollOffset object
+    /// </summary>
+    /// <param name="top">The top offset of content</param>
+    /// <param name="bottom">The bottom offset of content</param>
+    public VerticalScrollOffset(double top, double bottom)
+    {
+        this.Top = top;
+        this.Bottom = bottom;
+    }
 
 
-        /// <inheritdoc/>
-        public override string ToString()
-        {
-            return ToString(CultureInfo.InvariantCulture);
-        }
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        return $"{Top}, {Bottom}";
+    }
 
-        internal string ToString(CultureInfo cultureInfo)
-        {
-            char numericListSeparator = TokenizerHelper.GetNumericListSeparator(cultureInfo);
-            StringBuilder stringBuilder = new StringBuilder(64);
-            stringBuilder.Append(InternalToString(_Top, cultureInfo));
-            stringBuilder.Append(numericListSeparator);
-            stringBuilder.Append(InternalToString(_Bottom, cultureInfo));
-            return stringBuilder.ToString();
-        }
-
-        internal string InternalToString(double l, CultureInfo cultureInfo)
-        {
-            if (double.IsNaN(l))
-            {
-                return "Unset";
-            }
-
-            return Convert.ToString(l, cultureInfo);
-        }
-
-        /// <inheritdoc/>
-        public override bool Equals(object obj)
-        {
-            if (obj is VerticalScrollOffset verticalScrollOffset)
-            {
-                return this == verticalScrollOffset;
-            }
-
-            return false;
-        }
-        /// <summary>
-        /// Indicates whether this instance and a specified object are equal.
-        /// </summary>
-        /// <returns>true if verticalScrollOffset and this instance are represent the same value; otherwise, false.</returns>
-        public readonly bool Equals(VerticalScrollOffset verticalScrollOffset)
+    /// <inheritdoc/>
+    public override bool Equals(object obj)
+    {
+        if (obj is VerticalScrollOffset verticalScrollOffset)
         {
             return this == verticalScrollOffset;
         }
 
-        /// <inheritdoc/>
-        public override int GetHashCode()
+        return false;
+    }
+    
+    /// <summary>
+    /// Indicates whether this instance and a specified object are equal.
+    /// </summary>
+    /// <returns>true if verticalScrollOffset and this instance are represent the same value; otherwise, false.</returns>
+    public readonly bool Equals(VerticalScrollOffset verticalScrollOffset)
+    {
+        return this == verticalScrollOffset;
+    }
+
+    /// <inheritdoc/>
+    public override int GetHashCode()
+    {
+        return Top.GetHashCode() ^ Bottom.GetHashCode();
+    }
+
+    /// <summary>
+    /// Compares two VerticalScrollOffset objects
+    /// </summary>
+    /// <param name="t1"></param>
+    /// <param name="t2"></param>
+    /// <returns>true if offsets are same; otherwise, false.</returns>
+    public static bool operator ==(VerticalScrollOffset t1, VerticalScrollOffset t2)
+    {
+        if (t1.Top == t2.Top)
         {
-            return _Top.GetHashCode() ^ _Bottom.GetHashCode();
+            return t1.Bottom == t2.Bottom;
         }
 
-        /// <summary>
-        /// Compares two VerticalScrollOffset objects
-        /// </summary>
-        /// <param name="t1"></param>
-        /// <param name="t2"></param>
-        /// <returns>true if offsets are same; otherwise, false.</returns>
-        public static bool operator ==(VerticalScrollOffset t1, VerticalScrollOffset t2)
-        {
-            if (t1._Top == t2._Top)
-            {
-                return t1._Bottom == t2._Bottom;
-            }
+        return false;
+    }
 
-            return false;
-        }
-
-        /// <summary>
-        /// Compares two VerticalScrollOffset objects
-        /// </summary>
-        /// <param name="t1"></param>
-        /// <param name="t2"></param>
-        /// <returns>true if offsets are different; otherwise, false.</returns>
-        public static bool operator !=(VerticalScrollOffset t1, VerticalScrollOffset t2)
-        {
-            return !(t1 == t2);
-        }
+    /// <summary>
+    /// Compares two VerticalScrollOffset objects
+    /// </summary>
+    /// <param name="t1"></param>
+    /// <param name="t2"></param>
+    /// <returns>true if offsets are different; otherwise, false.</returns>
+    public static bool operator !=(VerticalScrollOffset t1, VerticalScrollOffset t2)
+    {
+        return !(t1 == t2);
     }
 }
